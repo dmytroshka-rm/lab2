a6dd8af82894d592e2b098d87a56a20c186f99ae
dmytroshka-rm <dimakipen2006@gmail.com>
2025-09-22 18:32:16 +0300

    lab2

---
+++ src/mainpackage/Main.java
@@ -0,0 +1,142 @@
+package mainpackage;
+import patientpackage.Patient;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Скільки пацієнтів ви хочете ввести?");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+        Patient[] patients = new Patient[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("\nВведення даних пацієнта №" + (i + 1));
+
+            System.out.print("Прізвище: ");
+            String lastName = scanner.nextLine();
+
+            System.out.print("Ім'я: ");
+            String firstName = scanner.nextLine();
+
+            System.out.print("По-батькові: ");
+            String secondName = scanner.nextLine();
+
+            System.out.print("Адреса: ");
+            String address = scanner.nextLine();
+
+            String number;
+            while (true) {
+                System.out.print("Телефон: ");
+                number = scanner.nextLine();
+                if (!existsPhone(number, patients, i)) {
+                    break;
+                }
+                System.out.println("Такий номер телефону вже існує.Введіть інший.");
+            }
+
+            int id;
+            while (true) {
+                System.out.print("ID: ");
+                id = scanner.nextInt();
+                scanner.nextLine();
+                if (!existsId(id, patients, i)) {
+                    break;
+                }
+                System.out.println("Такий ID вже існує.Введіть інший.");
+            }
+
+            int numMedCard;
+            while (true) {
+                System.out.print("Номер медичної картки: ");
+                numMedCard = scanner.nextInt();
+                scanner.nextLine();
+                if (!existsMedCard(numMedCard, patients, i)) {
+                    break;
+                }
+                System.out.println("Такий номер медичної картки вже існує.Введіть інший.");
+            }
+
+            System.out.print("Діагноз: ");
+            String diagnos = scanner.nextLine();
+
+            patients[i] = new Patient(id, firstName, secondName, lastName, address, number, numMedCard, diagnos);
+        }
+
+        System.out.print("\nВведіть діагноз для пошуку: ");
+        String searchDiag = scanner.nextLine();
+        printByDiagnos(searchDiag, patients);
+
+        System.out.print("\nВведіть початок діапазону номера медкартки: ");
+        int a = scanner.nextInt();
+        System.out.print("Введіть кінець діапазону номера медкартки: ");
+        int b = scanner.nextInt();
+        scanner.nextLine();
+        printByInterval(a, b, patients);
+
+        System.out.print("\nВведіть цифру, з якої починається телефон: ");
+        char digit = scanner.nextLine().charAt(0);
+        printByPhoneDigit(digit, patients);
+
+        scanner.close();
+    }
+
+    public static boolean existsId(int id, Patient[] patients, int count) {
+        for (int i = 0; i < count; i++) {
+            if (patients[i] != null && patients[i].getId() == id) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean existsPhone(String phone, Patient[] patients, int count) {
+        for (int i = 0; i < count; i++) {
+            if (patients[i] != null && patients[i].getNumber().equals(phone)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean existsMedCard(int num, Patient[] patients, int count) {
+        for (int i = 0; i < count; i++) {
+            if (patients[i] != null && patients[i].getNumMedCard() == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void printByDiagnos(String diag, Patient[] patients) {
+        System.out.println("\nПацієнти з діагнозом '" + diag + "':");
+        for (Patient p : patients) {
+            if (p.TrueDisgnos(diag)) {
+                System.out.println(p);
+            }
+        }
+    }
+
+    public static void printByInterval(int a, int b, Patient[] patients) {
+        System.out.println("\nПацієнти з номером медкартки у діапазоні [" + a + ", " + b + "]:");
+        for (Patient p : patients) {
+            if (p.interval(a, b)) {
+                System.out.println(p);
+            }
+        }
+    }
+
+    public static void printByPhoneDigit(char digit, Patient[] patients) {
+        int count = 0;
+        System.out.println("\nПацієнти, телефон яких починається з '" + digit + "':");
+        for (Patient p : patients) {
+            if (p.firstNumPhone(digit)) {
+                System.out.println(p);
+                count++;
+            }
+        }
+        System.out.println("Загальна кількість: " + count);
+    }
+
+}
---
+++ src/patientpackage/Patient.java
@@ -0,0 +1,96 @@
+package patientpackage;
+public class Patient {
+    private int id;
+    private String firstName;
+    private String secondName;
+    private String lastName;
+    private String address;
+    private String number;
+    private int numMedCard;
+    private String diagnos;
+
+    public Patient(int id, String firstName, String secondName, String lastName, String address, String number, int numMedCard, String diagnos) {
+        this.id = id;
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.lastName = lastName;
+        this.address = address;
+        this.number = number;
+        this.numMedCard = numMedCard;
+        this.diagnos = diagnos;
+    }
+
+    public int getId() { return id; }
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getSecondName() {
+        return secondName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public String getNumber() {
+        return number;
+    }
+    public int getNumMedCard() {
+        return numMedCard;
+    }
+    public String getDiagnos() {
+        return diagnos;
+    }
+
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setSecondName(String secondName) {
+        this.secondName = secondName; }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public void setNumber(String number) {
+        this.number = number;
+    }
+    public void setNumMedCard(int numMedCard) {
+        this.numMedCard = numMedCard;
+    }
+    public void setDiagnos(String diagnos) {
+        this.diagnos = diagnos;
+    }
+
+    public boolean firstNumPhone(char sumbol) {
+        return number.charAt(0) == sumbol;
+    }
+
+    public boolean interval(int a, int b) {
+        return numMedCard >= a && numMedCard <= b;
+    }
+
+    public boolean TrueDisgnos(String diag) {
+        return diagnos.equalsIgnoreCase(diag);
+    }
+
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", address='" + address + '\'' +
+                ", number='" + number + '\'' +
+                ", numMedCard=" + numMedCard +
+                ", diagnos='" + diagnos + '\'' +
+                '}';
+    }
+}
+
